8e5d6a7442334bd3b011f2705ffd53354189a687
BETOLPES1002 <a3520110125@alumno.uttehuacan.edu.mx>
2022-10-13 21:14:53 -0500

    Componentes vue

---
+++ src/components/Grid.vue
@@ -0,0 +1,16 @@
+<template>
+    <div></div>
+</template>
+<style scoped>
+div {
+    display: inline-block;
+    text-align: center;
+    box-shadow: 0px 0px 15px violet;
+    padding: 1rem;
+    border-end-start-radius: 10px;
+    border-start-start-radius: 10px;
+}
+</style>
+<script>
+
+</script>
